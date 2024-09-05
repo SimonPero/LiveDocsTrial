f868da26005d6ed52b7953795e6dc6292060a1d1
Simon Pero <simonraulpero@gmail.com>
2024-09-05 18:43:45 -0300

    Finished Docs and Home pages

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,22 @@
+import CollaborativeRoom from "@/components/CollaborativeRoom";
+import { getDocument } from "@/lib/actions/room.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
+export default async function Document({ params: { id } }: SearchParamProps) {
+  const clerkUser = await currentUser();
+  if (!clerkUser) redirect("sign-in");
 
-export default function Document() {
+  const room = await getDocument({
+    roomId: id,
+    userId: clerkUser?.emailAddresses[0].emailAddress,
+  });
+
   return (
-    <div>
-     
-    </div>
+    <main className="flex w-full flex-col items-center">
+      <CollaborativeRoom 
+        roomId={id} 
+        roomMetadata={room.metadata} />
+    </main>
   );
 }
--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Loader from "@/components/Loader";
+import { getClerkUsers } from "@/lib/actions/user.actions";
 import {
   ClientSideSuspense,
   LiveblocksProvider,
@@ -7,7 +8,13 @@ import {
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <LiveblocksProvider authEndpoint={"/api/liveblocks-auth"}>
+    <LiveblocksProvider
+      authEndpoint="/api/liveblocks-auth"
+      resolveUsers={async ({ userIds }) => {
+        const users = await getClerkUsers({ userIds });
+        return users;
+      }}
+    >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
   );
